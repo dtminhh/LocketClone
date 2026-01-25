f6fe5d71b998924b4689f4cb7cd167395c552c33
dtminh <minhm5993@gmail.com>
2026-01-25 21:23:41 +0700

    Feat: Show preview taken photo

--- app/src/main/java/com/mainpack/locketcameraclone/ui/fragment/mainFunc/CameraFragment.kt
+++ app/src/main/java/com/mainpack/locketcameraclone/ui/fragment/mainFunc/CameraFragment.kt
@@ -1,6 +1,8 @@
 package com.mainpack.locketcameraclone.ui.fragment.mainFunc
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
@@ -15,9 +19,15 @@ import androidx.fragment.app.Fragment
 import com.mainpack.locketcameraclone.databinding.FragmentCameraBinding
 import com.mainpack.locketcameraclone.ui.utils.PermissionUtils
 import com.mainpack.locketcameraclone.ui.utils.PermissionUtils.hasRequirePermission
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class CameraFragment : Fragment() {
     private var lensFacing = CameraSelector.LENS_FACING_FRONT
+    private var imageCapture: ImageCapture? = null
+
+    private var cameraProvider: ProcessCameraProvider? = null
+
     private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding!!
     private val requestPermissionLauncher = registerForActivityResult(
@@ -59,17 +69,23 @@ class CameraFragment : Fragment() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
 
         cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
+            cameraProvider = cameraProviderFuture.get()
 
             val preview = Preview.Builder().build().also {
                 it.surfaceProvider = binding.cameraView.surfaceProvider
             }
+            imageCapture = ImageCapture.Builder().build()
 
             val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
             try {
-                cameraProvider.unbindAll()
+                cameraProvider?.unbindAll()
 
-                cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview)
+                cameraProvider?.bindToLifecycle(
+                    viewLifecycleOwner,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
             } catch (exc: Exception) {
                 Log.e("CameraFragment", "Use case binding failed", exc)
             }
@@ -78,6 +94,15 @@ class CameraFragment : Fragment() {
 
     private fun setUpListener() {
         binding.apply {
+            shotBtn.setOnClickListener {
+                val bitmapPreview = cameraView.bitmap
+
+                if (bitmapPreview != null) {
+                    previewImg.setImageBitmap(bitmapPreview)
+                    previewImg.visibility = View.VISIBLE
+                    showPreviewUI()
+                }
+            }
             slipCameraBtn.setOnClickListener {
                 lensFacing = if (lensFacing == CameraSelector.LENS_FACING_FRONT) {
                     CameraSelector.LENS_FACING_BACK
@@ -86,6 +111,37 @@ class CameraFragment : Fragment() {
                 }
                 startCamera()
             }
+            cancelBtn.setOnClickListener {
+                showCameraUI()
+            }
         }
+    }
+
+    private fun showPreviewUI() {
+        cameraProvider?.unbindAll()
+        binding.apply {
+            cameraGroup.visibility = View.GONE
+            previewModeBtnGroup.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showCameraUI() {
+        startCamera()
+        binding.apply {
+            cameraGroup.visibility = View.VISIBLE
+            cameraView.visibility = View.VISIBLE
+            previewModeBtnGroup.visibility = View.GONE
+            root.postDelayed(
+                {
+                    previewImg.visibility = View.GONE
+                }, DELAY_VALUE
+            )
+            previewImg.setImageBitmap(null)
+        }
+    }
+
+    companion object {
+        private const val FILE_NAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val DELAY_VALUE = 500L
     }
 }
